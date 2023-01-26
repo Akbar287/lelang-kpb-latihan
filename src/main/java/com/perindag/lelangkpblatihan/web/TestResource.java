dc9f2d8dbf4f08d19e30fc7de0f993df62e483e4
Akbar287 <49776425+Akbar287@users.noreply.github.com>
2023-01-26 20:00:55 +0700

    Update Route

--- src/main/java/com/perindag/lelangkpblatihan/web/TestResource.java
+++ src/main/java/com/perindag/lelangkpblatihan/web/TestResource.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class TestResource {
     @GetMapping("/")
     public ResponseEntity<String> get() {
